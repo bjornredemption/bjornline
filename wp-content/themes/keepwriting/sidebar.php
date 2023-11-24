749f6f89c4544f9fba99bd699aeafdd7b90fdc7f
Bjorn De Lima <bjornishere@gmail.com>
2023-11-24 22:30:41 +1100

    2023 restart

--- wp-content/themes/keepwriting/sidebar.php
+++
@@ -1,5 +0,0 @@
-    <ul class="sidebar">
-    <?php if ( function_exists('dynamic_sidebar') && dynamic_sidebar('bottomsection') ) : else : ?>
-    <p>Please configure this section through Widgets</p>        
-    <?php endif; ?>
-    </ul>
